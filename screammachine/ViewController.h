edcfa0216658861b8958cec97962ff6691203c7f
Pulkit Singhal <pulkitsinghal@gmail.com>
2012-03-01 07:52:47 -0600

    add images on the fly

--- screammachine/ViewController.h
+++ screammachine/ViewController.h
@@ -9,5 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@property (nonatomic, retain) UIImagePickerController   *imagePickerController;
+@property (nonatomic, retain) UIButton                  *thisButtonNeedsPicture;
+
+- (IBAction) showPopUp : (id) sender;
+
+- (IBAction) takePicture : (id) sender;
 
 @end
--- screammachine/ViewController.m
+++ screammachine/ViewController.m
@@ -7,8 +7,28 @@
 //
 
 #import "ViewController.h"
+#import "UIImage+Utility.h"
 
 @implementation ViewController
+
+#pragma mark - Instance variables
+@synthesize imagePickerController  = _imagePickerController;
+@synthesize thisButtonNeedsPicture = _thisButtonNeedsPicture;
+
+#pragma mark - NSObject methods
+- (id) initWithNibName : (NSString *) nibNameOrNil
+                bundle : (NSBundle *) nibBundleOrNil
+{
+    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]))
+    {
+        self.imagePickerController = [[UIImagePickerController alloc] init];
+        if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+            self.imagePickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+        }
+        self.imagePickerController.delegate = self;
+    }
+    return self;
+}
 
 - (void)didReceiveMemoryWarning
 {
@@ -16,8 +36,78 @@
     // Release any cached data, images, etc that aren't in use.
 }
 
-#pragma mark - View lifecycle
+#pragma mark - Instance methods
+- (IBAction) showPopUp : (id) sender
+{
+    UIAlertView *popUp =
+    [[UIAlertView alloc] initWithTitle : @"Guess Who?"
+                               message : ((UIButton*)sender).titleLabel.text
+                              delegate : nil
+                     cancelButtonTitle : @"No Idea!"
+                     otherButtonTitles : nil];
+    [popUp show];
+}
 
+- (IBAction) takePicture : (id) sender
+{
+    if (((UIButton*)sender).imageView.image == nil) {
+        [self presentViewController:self.imagePickerController
+                           animated:YES
+                         completion:
+                         ^{
+                             NSLog(@"This runs before the camera shutter opens :)");
+                             self.thisButtonNeedsPicture = (UIButton*)sender;
+                         }
+        ];
+    } else {
+        [self showPopUp:sender];
+    }
+}
+
+#pragma mark - UIImagePickerControllerDelegate methods
+- (void) imagePickerController : (UIImagePickerController *) picker
+         didFinishPickingImage : (UIImage *) image
+{
+    NSLog(@"picked a picture");
+
+    //self.thisButtonNeedsPicture.imageView.image =
+    image =
+    [UIImage imageWithImage:image
+               scaledToSize:(CGSizeMake(85.0, 122.0))];
+
+    [self.thisButtonNeedsPicture setImage:image
+                                 forState:UIControlStateNormal];
+
+    [self dismissModalViewControllerAnimated:YES];
+}
+
+- (void) imagePickerController : (UIImagePickerController *) picker
+ didFinishPickingMediaWithInfo : (NSDictionary *) info
+{
+    NSLog(@"took a picture");
+
+    NSLog(@"%@", self.thisButtonNeedsPicture);
+    NSLog(@"%@", self.thisButtonNeedsPicture.imageView);
+    NSLog(@"%@", self.thisButtonNeedsPicture.imageView.image);
+
+    //self.thisButtonNeedsPicture.imageView.image =
+    UIImage *image =
+    [UIImage imageWithImage:(UIImage *) [info objectForKey:UIImagePickerControllerOriginalImage]
+               scaledToSize:(CGSizeMake(85.0, 122.0))];
+
+    [self.thisButtonNeedsPicture setImage:image
+                                 forState:UIControlStateNormal];
+
+    [self dismissModalViewControllerAnimated:YES];
+}
+
+- (void)imagePickerControllerDidCancel : (UIImagePickerController *) picker
+{
+    NSLog(@"did not take/pick a picture");
+    [self dismissModalViewControllerAnimated:YES];
+}
+
+#pragma mark - View lifecycle
 - (void)viewDidLoad
 {
     [super viewDidLoad];
